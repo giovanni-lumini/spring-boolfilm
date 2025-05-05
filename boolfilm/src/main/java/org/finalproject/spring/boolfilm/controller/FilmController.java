72e03deb0b36d1476cf801354aab843cf9daa20c
giovanni-lumini <giovanni.lumini28@gmail.com>
2025-05-05 16:32:24 +0200

    create and edit

--- boolfilm/src/main/java/org/finalproject/spring/boolfilm/controller/FilmController.java
+++ boolfilm/src/main/java/org/finalproject/spring/boolfilm/controller/FilmController.java
@@ -8,9 +8,17 @@ import org.finalproject.spring.boolfilm.repository.FilmRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/film")
@@ -49,6 +57,65 @@ public class FilmController {
         model.addAttribute("film", optionalFilm.get());
 
         return "film/show";
+    }
+
+    // CREATE
+    @GetMapping("/create")
+    public String create(Model model) {
+
+        // add a new empty object film
+        model.addAttribute("film", new Film());
+
+        return "film/create-or-edit";
+    }
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute Film formFilm, BindingResult bindingResult) {
+
+        // if validation errors are present
+        if (bindingResult.hasErrors()) {
+            return "film/create-or-edit";
+        }
+
+        // else, save
+        filmRepository.save(formFilm);
+
+        return "redirect:/film";
+    }
+
+    // UPDATE
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") Integer id, Model model) {
+
+        // find id
+        Optional<Film> optionalFilm = filmRepository.findById(id);
+
+        // if id is empty
+        if (optionalFilm.isEmpty()) {
+            return "film/filmNotFound";
+        }
+
+        // if id exists
+        model.addAttribute("film", optionalFilm.get());
+
+        // edit=true, for the form
+        model.addAttribute("edit", true);
+
+        return "film/create-or-edit";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String update(@Valid @ModelAttribute Film formFilm, BindingResult bindingResult) {
+
+        // if validation errors are present
+        if (bindingResult.hasErrors()) {
+            return "film/create-or-edit";
+        }
+
+        // else, save
+        filmRepository.save(formFilm);
+
+        return "redirect:/film";
     }
 
 }
