01ddc84ecbec69261f1bae2d67373a6d9f7dba9b
giovanni-lumini <giovanni.lumini28@gmail.com>
2025-05-05 08:43:14 +0200

    start

---
+++ boolfilm/src/main/java/org/finalproject/spring/boolfilm/BoolfilmApplication.java
@@ -0,0 +1,13 @@
+package org.finalproject.spring.boolfilm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoolfilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoolfilmApplication.class, args);
+	}
+
+}
---
+++ boolfilm/src/test/java/org/finalproject/spring/boolfilm/BoolfilmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.finalproject.spring.boolfilm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoolfilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
