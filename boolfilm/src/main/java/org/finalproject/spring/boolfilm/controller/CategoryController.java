df2bd1abc263335d7a4a74acd385442bb4f87b93
giovanni-lumini <giovanni.lumini28@gmail.com>
2025-05-06 10:07:07 +0200

    show

--- boolfilm/src/main/java/org/finalproject/spring/boolfilm/controller/CategoryController.java
+++ boolfilm/src/main/java/org/finalproject/spring/boolfilm/controller/CategoryController.java
@@ -1,14 +1,18 @@
 package org.finalproject.spring.boolfilm.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.finalproject.spring.boolfilm.model.Category;
 import org.finalproject.spring.boolfilm.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 @RequestMapping("/categories")
@@ -32,6 +36,22 @@ public class CategoryController {
     }
 
     // SHOW
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") Integer id, Model model) {
+
+        // find id
+        Optional<Category> categoryOptional = categoryRepository.findById(id);
+
+        // if film doesn't exist, exeption 404
+        if (categoryOptional.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Film not found");
+        }
+
+        // else
+        model.addAttribute("category", categoryOptional.get());
+
+        return "category/show";
+    }
 
     // CREATE
 
