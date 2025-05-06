abfe02cbe1d1aa9ef4d3044eba3d889c149d691e
giovanni-lumini <giovanni.lumini28@gmail.com>
2025-05-06 16:28:55 +0200

    style

--- boolfilm/src/main/java/org/finalproject/spring/boolfilm/controller/HomeController.java
+++ boolfilm/src/main/java/org/finalproject/spring/boolfilm/controller/HomeController.java
@@ -1,6 +1,8 @@
 package org.finalproject.spring.boolfilm.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,7 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequestMapping("/")
 public class HomeController {
     @GetMapping
-    public String home() {
+    public String home(Model model, Authentication authentication) {
+        // authetication name
+        model.addAttribute("username", authentication.getName());
         return "home";
     }
 
