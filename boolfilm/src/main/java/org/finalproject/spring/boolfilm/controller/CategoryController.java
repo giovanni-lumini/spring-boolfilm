f56965447095d06ae43a5d538de22824ad393ec8
giovanni-lumini <giovanni.lumini28@gmail.com>
2025-05-06 10:24:08 +0200

    create and edit

--- boolfilm/src/main/java/org/finalproject/spring/boolfilm/controller/CategoryController.java
+++ boolfilm/src/main/java/org/finalproject/spring/boolfilm/controller/CategoryController.java
@@ -9,10 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequestMapping("/categories")
@@ -42,9 +48,9 @@ public class CategoryController {
         // find id
         Optional<Category> categoryOptional = categoryRepository.findById(id);
 
-        // if film doesn't exist, exeption 404
+        // if category doesn't exist, exeption 404
         if (categoryOptional.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Film not found");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found");
         }
 
         // else
@@ -54,8 +60,65 @@ public class CategoryController {
     }
 
     // CREATE
+    @GetMapping("/create")
+    public String create(Model model) {
+
+        // add a new empty object Category
+        Category category = new Category();
+
+        model.addAttribute("category", category);
+
+        return "category/create-or-edit";
+    }
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute Category formCategory, BindingResult bindingResult) {
+
+        // if validation errors are present
+        if (bindingResult.hasErrors()) {
+            return "category/create-or-edit";
+        }
+
+        // else, save
+        categoryRepository.save(formCategory);
+
+        return "redirect:/categories";
+    }
 
     // UPDATE
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") Integer id, Model model) {
+
+        // find id
+        Optional<Category> categoryOptional = categoryRepository.findById(id);
+
+        // if category doesn't exist, exeption 404
+        if (categoryOptional.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found");
+        }
+
+        // else
+        model.addAttribute("category", categoryOptional.get());
+
+        // edit=true, for the form
+        model.addAttribute("edit", true);
+
+        return "category/create-or-edit";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String update(@Valid @ModelAttribute Category formCategory, BindingResult bindingResult) {
+
+        // if validation errors are present
+        if (bindingResult.hasErrors()) {
+            return "category/create-or-edit";
+        }
+
+        // else, save
+        categoryRepository.save(formCategory);
+
+        return "redirect:/categories";
+    }
 
     // DELETE
 }
