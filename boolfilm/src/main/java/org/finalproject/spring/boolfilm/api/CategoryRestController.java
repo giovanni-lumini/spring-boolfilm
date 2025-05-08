01f8bb168b0a3d612fc46a69decc7ec39d041e6e
giovanni-lumini <giovanni.lumini28@gmail.com>
2025-05-08 09:41:34 +0200

    fix rest controller for the api call from react

--- boolfilm/src/main/java/org/finalproject/spring/boolfilm/api/CategoryRestController.java
+++ boolfilm/src/main/java/org/finalproject/spring/boolfilm/api/CategoryRestController.java
@@ -13,6 +13,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/categories")
+@CrossOrigin(origins = "http://localhost:5173") // for react app
 public class CategoryRestController {
 
     // SERVICE
